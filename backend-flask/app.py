34a349882dc97cc00a04e84461ed1ca8b0749682
Noodles-boop <Oldoni.jeremy@outlook.fr>
2023-03-07 19:56:55 +0000

    xray setup in progress

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -25,7 +25,7 @@ from services.create_message import *
 from services.show_activity import *
 from services.notifications_activities import *
 
-#  HeneyComb
+#  HeneyComb ----------
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
@@ -33,10 +33,20 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
+# X-RAY -------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+# X-RAY -------
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 

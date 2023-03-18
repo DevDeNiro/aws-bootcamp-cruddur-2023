b7816c6c003ac18d85eccbfeb525dfc6a8be4e10
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2023-03-18 14:20:29 +0100

    Update jwtVerifier.js

--- jwt-verification-process/jwtVerifier.js
+++ jwt-verification-process/jwtVerifier.js
@@ -6,16 +6,42 @@ const userpoolId = process.env.COGNITO_USERPOOL_ID;
 
 const cognitoidentityserviceprovider = new CognitoIdentityServiceProvider({region});
 
+// Error Handling
 const verifyJwt = async (jwtToken) => {
-  const decoded = decode(jwtToken);
+  if (!jwtToken) {
+    throw new Error('JWT token is required.');
+  }
+
+  const decoded = decode(jwtToken, {complete: true});
+
+  if (!decoded || !decoded.payload) {
+    throw new Error('Invalid JWT token.');
+  }
+
+  const {header, payload} = decoded;
+ 
   const params = {
     AccessToken: jwtToken,
   };
-  const result = await cognitoidentityserviceprovider.getUser(params).promise();
-  const username = result.Username;
-  const userpool = result.UserPoolId;
-  const isValid = userpool === userpoolId && username === decoded['cognito:username'];
-  return {isValid, decoded};
+  
+  try {
+    const result = await cognitoidentityserviceprovider.getUser(params).promise();
+    const username = result.Username;
+    const userpool = result.UserPoolId;
+
+    if (userpool !== userpoolId) {
+      throw new Error('Invalid user pool ID.');
+    }
+
+    if (username !== payload['cognito:username']) {
+      throw new Error('Invalid username.');
+    }
+
+    return {isValid: true, decoded: payload};
+  } catch (error) {
+    throw new Error(`Failed to verify JWT token: ${error.message}`);
+  }
 };
 
 export default {verifyJwt};
+
